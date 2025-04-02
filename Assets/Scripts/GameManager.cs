e67cc02eb2330580538c1e1c26260801b333abb5
Rhenn3D <aalvarez@ctsformacion.com>
2025-04-02 10:51:10 +0200

    menu y flechitas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public bool isPlaying = true;
     public bool isPaused = false;
     private SoundManager soundManager;
+    public GameObject pauseCanvas;
     void Awake()
     {
         soundManager = FindObjectOfType<SoundManager>().GetComponent<SoundManager>();
@@ -16,21 +18,28 @@ public class GameManager : MonoBehaviour
         if(Input.GetButtonDown("Pause"))
         {
             Pause();
-        }        
+        }               
     }
-    void Pause()
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+    public void Pause()
     {
         if(isPaused)
         {
             Time.timeScale = 1;
             isPaused = false;
             soundManager.PauseBGM();
+            pauseCanvas.SetActive(false);
         }
         else
         {
             Time.timeScale = 0;
             isPaused = true;
             soundManager.PauseBGM();
+            pauseCanvas.SetActive(true);
         }
     }
 }
